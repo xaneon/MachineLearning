4d8940536e6d244fb6b9c3675edd64353afb1be6
bonne <bonne.habekost@gmail.com>
2019-07-21 18:53:10 +0200

    added distribution plots for word lengths

--- examples/record_linkage.py
+++ examples/record_linkage.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import re
 
 # 3 examples (Ex1-3) of Matching
 # Ex 1: Data integration - Data from multiple sources
@@ -71,4 +72,50 @@ fields_wage = ["trade_nm", "legal_name", "street_addr_1_txt", "cty_nm",
 df_wage_a = df_wage[fields_wage].drop_duplicates()
 df_wage_a.info()
 
-# TODO: apply and groupby
+# Transformations: apply and groupby
+# different zip code formats:
+t = {ord(x): y for (x, y) in zip("0123456789", "999999999")}
+print(t)
+
+def zip_trans(z): return str(z).translate(t) if pd.notnull(z) else z
+
+df_inspection_a["zip_code_pattern"] = df_inspection_a.location_zip_code.apply(zip_trans)
+print(df_inspection_a.groupby("zip_code_pattern").size())
+
+df_wage_a["zip_code_pattern"] = df_wage_a.zip_cd.apply(zip_trans)
+print(df_wage_a.groupby("zip_code_pattern").size())
+
+# apply with regular expressions:
+def leading_patt(s):
+    p = (("^[A-Z][a-z]", "Aa"), ("^[A-Z][A-Z]", "AA"), ("^[0-9][0-9]", "99"))
+    for rx, patt in p:
+        if re.search(rx, s):
+            return patt
+    return "OTHER"
+
+df_wage_a["leading_pattern"] = df_wage_a.trade_nm.apply(leading_patt)
+print(df_wage_a.groupby("leading_pattern").size())
+
+# can be used to check whether dates or emails are in a particular format
+
+# Filter with lambda => group by having in SQL
+# cases where company address and city are the same, looking for duplicates
+df = df_inspection_a.groupby(["dba", "location_zip_code"])\
+    .filter(lambda x: len(x["dba"]) > 2)\
+    .sort_values(by="dba")[["dba", "location_address", "location_zip_code",
+                            "district", "county_number"]].head(10)
+print(df.head(5))
+
+# How about ambigious legal names?
+print(df_wage_a.legal_name.value_counts(dropna=True).head())
+
+# But trade names quite business names:
+print(df_wage_a.trade_nm.value_counts(dropna=True).head(5))
+
+# Histogram of matching
+plt.figure()
+plt.hist(df_inspection_a.dba.apply(lambda x: len(x) if pd.notnull(x) else 0),
+         bins=50, color="red", alpha=0.3)
+plt.hist(df_wage_a.trade_nm.apply(lambda x: len(x) if pd.notnull(x) else 0),
+         bins=50, color="blue", alpha=0.3)
+plt.savefig("test_word_length_distributions.png")
