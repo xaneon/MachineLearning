c0219e2a3f531a99b0ee214f6e5a8401f3ca682c
Bonne <bonne.habekost@gmail.com>
2019-09-23 11:10:58 +0200

    added a wrapper plot function to clean it a bit up

--- visualisation/visualisationtypes.py
+++ visualisation/visualisationtypes.py
@@ -5,14 +5,38 @@ from numpy.random import randint, randn
 num = 42
 years = [randint(1950, 2010) for i in range(num)]
 gdp = [round(randint(300, 15_000) + randn(1)[0], 2)
-       for i in range(num)]
+       for _ in range(num)]
+movies = ["".join([chr(randint(0, 2**8))
+          for _ in range(randint(6, 14))])
+          for _ in range(num)]
+num_oscars = [randint(0, 42) for _ in range(num)]
+xs = [i + 0.1 for i, _ in enumerate(movies)]
+
+def plot(func, *args, **kwargs):
+    plt.figure()
+    savefile = kwargs.pop("savefile")
+    xlabel = kwargs.pop("xlabel")
+    ylabel = kwargs.pop("ylabel")
+    freturn = func(*args, **kwargs)
+    plt.xlabel(xlabel); plt.ylabel(ylabel)
+    plt.savefig(savefile)
+    return freturn
+
 
 def line(x, y, *params, **kwargs):
-    plt.plot(x, y, **kwargs)
-    plt.title("xlabel"); plt.ylabel("ylabel")
-    plt.savefig("lineplot.png")
+    return plt.plot(x, y, **kwargs)
+
+def bar(x, y, *params, **kwargs):
+    return plt.bar(x, y, **kwargs)
 
 if __name__ == "__main__":
     # Line chart
-    line(sorted(years), sorted(gdp), color="green",
-         marker="o", linestyle="solid")
+    plot(line, x=sorted(years), y=sorted(gdp),
+         color="green", marker="o", linestyle="solid",
+         savefile="lineplot.png", xlabel="Years",
+         ylabel="GDP")
+
+    plot(bar, x=xs, y=num_oscars, color="blue",
+         savefile="barplot.png", xlabel="Movies",
+         ylabel="# Oscars")
+
