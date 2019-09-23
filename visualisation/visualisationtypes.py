d77c1f4a1d623ece2bb2da5f90e56e4090bdb851
Bonne <bonne.habekost@gmail.com>
2019-09-23 10:41:22 +0200

    cleaned up the line chart

--- visualisation/visualisationtypes.py
+++ visualisation/visualisationtypes.py
@@ -7,8 +7,12 @@ years = [randint(1950, 2010) for i in range(num)]
 gdp = [round(randint(300, 15_000) + randn(1)[0], 2)
        for i in range(num)]
 
-plt.plot(sorted(years), sorted(gdp),
-         color="green", marker="o", linestyle="solid")
-plt.title("Nominal GDP")
-plt.ylabel("Billions of $")
-plt.savefig("lineplot.png")
+def line(x, y, *params, **kwargs):
+    plt.plot(x, y, **kwargs)
+    plt.title("xlabel"); plt.ylabel("ylabel")
+    plt.savefig("lineplot.png")
+
+if __name__ == "__main__":
+    # Line chart
+    line(sorted(years), sorted(gdp), color="green",
+         marker="o", linestyle="solid")
