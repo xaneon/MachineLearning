a42021384749bc27e28c5c5ee5326b42324937e4
Bonne <bonne.habekost@gmail.com>
2019-09-23 11:42:08 +0200

    started to implement my own histogram function

--- visualisation/visualisationtypes.py
+++ visualisation/visualisationtypes.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from numpy.random import randint, randn
+from collections import Counter
 
 num = 42
 years = [randint(1950, 2010) for i in range(num)]
@@ -11,16 +12,23 @@ movies = ["".join([chr(randint(0, 2**8))
           for _ in range(num)]
 num_oscars = [randint(0, 42) for _ in range(num)]
 xs = [i + 0.1 for i, _ in enumerate(movies)]
+grades = [randint(0, 100) for _ in range(num)]
+
+def myhist(data, binsize):
+    tobins = lambda elem: elem // binsize * binsize
+    values_counts = Counter(tobins(dt) for dt in data)
+    return values_counts
 
 def plot(func, *args, **kwargs):
     plt.figure()
+    fig, ax = plt.subplots(nrows=1, ncols=1)
     savefile = kwargs.pop("savefile")
     xlabel = kwargs.pop("xlabel")
     ylabel = kwargs.pop("ylabel")
     freturn = func(*args, **kwargs)
     plt.xlabel(xlabel); plt.ylabel(ylabel)
     plt.savefig(savefile)
-    return freturn
+    return fig, ax
 
 
 def line(x, y, *params, **kwargs):
@@ -31,12 +39,14 @@ def bar(x, y, *params, **kwargs):
 
 if __name__ == "__main__":
     # Line chart
-    plot(line, x=sorted(years), y=sorted(gdp),
-         color="green", marker="o", linestyle="solid",
-         savefile="lineplot.png", xlabel="Years",
-         ylabel="GDP")
+    p1, ax1 = plot(line, x=sorted(years), y=sorted(gdp),
+                   color="green", marker="o", linestyle="solid",
+                   savefile="lineplot.png", xlabel="Years",
+                   ylabel="GDP")
 
-    plot(bar, x=xs, y=num_oscars, color="blue",
-         savefile="barplot.png", xlabel="Movies",
-         ylabel="# Oscars")
+    p2, ax2 = plot(bar, x=xs, y=num_oscars, color="blue",
+                   savefile="barplot.png", xlabel="Movies",
+                   ylabel="# Oscars")
+
+    print(myhist(grades, 100))
 
