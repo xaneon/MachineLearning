bd9f64b62490c73fa71b549c6728ed5096414b7e
bonne <bonne.habekost@gmail.com>
2019-09-23 20:16:02 +0200

    added a few matrix functions.

--- mathematics/algebra/matrices.py
+++ mathematics/algebra/matrices.py
@@ -11,7 +11,29 @@ def shape(A):
      num_columns = len(A[0]) if A else 0
      return num_rows, num_columns
 
+
+def get_row(A, i):
+     return A[i]  # i-th row
+
+
+def get_column(A, j):
+     return [A_i[j] for A_i in A]  # j-th element of each row
+
+
+def make_matrix(num_rows, num_cols, entry_fn):
+     return [[entry_fn(i, j) for j in range(num_cols)] for i in range(num_rows)]
+
+def is_diagonal(i, j):
+     return 1 if i == j else 0
+
+
 if __name__ == "__main__":
-     pprint(A)
-     pprint(B)
+     pprint(A, width=20)
+     pprint(B, width=20)
      print(shape(A), shape(B))
+     print(get_row(A, 0))
+     print(get_row(A, 1))
+     print(get_column(A, 0))
+     print(get_column(A, 1))
+     pprint(make_matrix(3, 3, lambda a, b: a+b), width=20)
+     pprint(make_matrix(4, 4, is_diagonal), width=20)
