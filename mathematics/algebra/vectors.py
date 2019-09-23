4aedbc44e98283ce2f95517d6de70bc49177151c
Bonne <bonne.habekost@gmail.com>
2019-09-23 14:57:51 +0200

    added basic vector implementation with python lists

---
+++ mathematics/algebra/vectors.py
@@ -0,0 +1,22 @@
+height_weigt_age = [180,  # cm
+                    72,  # kg
+                    40]  # yrs
+
+grades = [95,  # exam 1
+          80,  # exam 2
+          77,  # exam 3
+          99]  # exam 4
+
+v = [1, 3, 9]
+w = [4, 5, 6]
+
+def vector_add(v, w):
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+if __name__ == "__main__":
+    print(height_weigt_age)
+    print(grades)
+    print(v)
+    print(w)
+    print(vector_add(v, w))
+
