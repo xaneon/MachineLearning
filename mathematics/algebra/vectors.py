3c960c65d5318f8fcd4b0b5d56e166ae04a865c5
Bonne <bonne.habekost@gmail.com>
2019-09-23 15:07:01 +0200

    prepared some vector basics

--- mathematics/algebra/vectors.py
+++ mathematics/algebra/vectors.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 height_weigt_age = [180,  # cm
                     72,  # kg
                     40]  # yrs
@@ -10,8 +12,14 @@ grades = [95,  # exam 1
 v = [1, 3, 9]
 w = [4, 5, 6]
 
+
 def vector_add(v, w):
     return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+
+def vector_subtract(v, w):
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
 
 if __name__ == "__main__":
     print(height_weigt_age)
@@ -19,4 +27,5 @@ if __name__ == "__main__":
     print(v)
     print(w)
     print(vector_add(v, w))
+    print(vector_subtract(v, w))
 
