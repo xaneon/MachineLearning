2773e920f0acb91f91b8037c90c658471fda069b
Bonne <bonne.habekost@gmail.com>
2019-09-24 15:10:46 +0200

    added dice and flipping coin example

--- mathematics/probabilities/probabilities_and_bayes.py
+++ mathematics/probabilities/probabilities_and_bayes.py
@@ -1,13 +1,14 @@
 from numpy.random import random, randn, choice, randint
 
 P1 = P2 = P3 = P4 = P5 = P6 = 1/6
+P_up = P_down = 1/2
 
 def roll_dice():
     return int(random(1) * 6) + 1
 
 def toss_coin():
-    # return choice(["head", "tail"], p=[1/2, 1/2])
-    return choice([0, 1], p=[1/2, 1/2])
+    return choice(["heads", "tails"], p=[1/2, 1/2])
+    # return choice([0, 1], p=[1/2, 1/2])
 
 
 if __name__ == "__main__":
@@ -19,4 +20,6 @@ if __name__ == "__main__":
     print(toss_coin())
     print(f"Probability of 1 and 2: {P1 * P2}")
     print(f"Throwing the two dices: {(roll_dice(), roll_dice())}")
+    print(f"Probability of two dices facing up: {P_up * P_down}")
+    print(f"Throwing two coins: {toss_coin(), toss_coin()}")
 
