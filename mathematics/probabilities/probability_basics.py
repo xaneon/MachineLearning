16ab1bf0726b2f7709761430d25d0c82c4110c8c
Bonne <bonne.habekost@gmail.com>
2019-09-24 11:05:18 +0200

    made a start with some basic probabilities

---
+++ mathematics/probabilities/probability_basics.py
@@ -0,0 +1,15 @@
+from numpy.random import randint, random, randn
+
+P_1 = 1/6  # probability that 1 occurs
+P_2 = P_3 = P_4 = P_5 = P_6 = 1/6
+
+P_12 = P_1 * P_2  # probability that 1 and 2 occurs
+
+def roll_dice():
+    return int(random(1) * 6) + 1
+
+if __name__ == "__main__":
+    print(f"Wahrscheintlichkeit für 1: {P_1}")
+    print(f"Wahrscheintlichkeit für 2: {P_2}")
+    print(f"Wahrscheintlichkeit für 1+2: {P_12}")
+    print(f"Würfeln ergab: {roll_dice()}")
