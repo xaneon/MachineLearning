4040be040999d6ee86801eb1981b65a46112a3a8
bhabekost <bonne.habekost@gmail.com>
2019-10-29 16:57:48 +0100

    Added possibility to solve Ax = b

--- examples/MNIST/analysis_from_scratch.py
+++ examples/MNIST/analysis_from_scratch.py
@@ -15,9 +15,13 @@ if __name__ == "__main__":
     data = loadall(datadir, prefix="*ubyte*")
     X_train, X_test = data["i60000"], data["i10000"]
     y_train, y_test = data["l60000"], data["l10000"]
-    mdobj = md(describe_matrix(X_train))  # non-square matrix
-                                     # => no solution to Ax = b
-                                     # What about A^T A x_hat = A^T b ?
+    mdobj = md(describe_matrix(X_train))
+    # non-square matrix
+    # => no solution to Ax = b
+    # What about A^T A x_hat = A^T b ?
+    # If there would be a solution, it could be solved with:
+    # np.linalg.solve(X_train, y_test)
+
     print(mdobj._repr_markdown_())
     # 1. Visualise and clean data
     plt.figure()
